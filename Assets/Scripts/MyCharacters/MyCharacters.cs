87bca2e16e35a60ca058af63ea372c8ab7901847
TetsuFe <tetsubeg1ner27@gmail.com>
2020-10-11 13:07:41 +0900

    Display my character

--- Assets/Scripts/MyCharacters/MyCharacters.cs
+++ Assets/Scripts/MyCharacters/MyCharacters.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 [Serializable]
@@ -19,8 +20,11 @@ class CharacterData
     public int user_id;
     public int characterId;
 }
+
 public class MyCharacters : MonoBehaviour
 {
+    [SerializeField] Canvas canvas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,9 +43,23 @@ public class MyCharacters : MonoBehaviour
         Debug.Log(request.downloadHandler.text);
         foreach (var e in myCharacters.myCharacters)
         {
-            Debug.Log(e.characterId);            
+            Debug.Log(e.characterId);
+            GameObject imgObject = new GameObject("testAAA");
+
+            RectTransform trans = imgObject.AddComponent<RectTransform>();
+            trans.transform.SetParent(canvas.transform); // setting parent
+            trans.localScale = Vector3.one;
+            trans.anchoredPosition = new Vector2(0f, 0f); // setting position, will be on center
+            trans.sizeDelta = new Vector2(150, 200); // custom size
+
+            Image image = imgObject.AddComponent<Image>();
+            imgObject.transform.SetParent(canvas.transform);
+            string path = "hokuma_" + e.characterId;
+            image.sprite = Resources.Load<Sprite>(path);
         }
     }
+
+    private Image image;
 
     // Update is called once per frame
     void Update()
