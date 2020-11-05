cc9bf72981f986bc2462463d4363c1fd5a25c1ae
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-06 05:33:26 +0900

    Add move to myCharacters scene button to menu scene

---
+++ Assets/Scripts/MenuScene/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button moveToMyCharactersButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMyCharactersScene()
+    {
+        SceneManager.LoadScene("MyCharactersScene");
+    }
+}
