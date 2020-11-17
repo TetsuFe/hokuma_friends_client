ef09454a1b7ce46b69a0be84b3e00239b35680eb
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-17 18:59:57 +0900

    Add story and story list scene

---
+++ Assets/Scripts/Story/StoryListController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Story
+{
+    public class StoryListController : MonoBehaviour
+    {
+        [SerializeField] private Button moveToStoryButton;
+        [SerializeField] private Button backToMenuButton;
+
+        void Start()
+        {
+            moveToStoryButton.onClick.AddListener(LoadStoryScene);
+            backToMenuButton.onClick.AddListener(LoadMenuScene);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        void LoadStoryScene()
+        {
+            SceneManager.LoadScene("StoryScene");
+        }
+
+        void LoadMenuScene()
+        {
+            SceneManager.LoadScene("MenuScene");
+        }
+    }
+}
