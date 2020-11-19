10429bb137c71cd999d1b39c479e3fd4073f155e
TetsuFe <tetsubeg1ner27@gmail.com>
2020-11-20 08:14:49 +0900

    Add dialog prefab

---
+++ Assets/Scripts/Dialog/OkCancelDialog.cs
@@ -0,0 +1,34 @@
+﻿// OkCancelDialog.cs
+
+using System;
+using UnityEngine;
+
+namespace Dialog
+{
+    public class OkCancelDialog : MonoBehaviour
+    {
+        public enum DialogResult
+        {
+            OK,
+            Cancel,
+        }
+
+        // ダイアログが操作されたときに発生するイベント
+        public Action<DialogResult> FixDialog { get; set; }
+
+        // OKボタンが押されたとき
+        public void OnOk()
+        {
+            this.FixDialog?.Invoke(DialogResult.OK);
+            Destroy(this.gameObject);
+        }
+
+        // Cancelボタンが押されたとき
+        public void OnCancel()
+        {
+            // イベント通知先があれば通知してダイアログを破棄してしまう
+            this.FixDialog?.Invoke(DialogResult.Cancel);
+            Destroy(this.gameObject);
+        }
+    }
+}
