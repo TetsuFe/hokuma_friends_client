c2cebaf63230eac184b09461ed3615465c6f95e5
TetsuFe <tetsubeg1ner27@gmail.com>
2020-12-11 08:54:19 +0900

    Show story by sql data after move from story list buttons

--- Assets/Scripts/Story/StoryListController.cs
+++ Assets/Scripts/Story/StoryListController.cs
@@ -11,6 +11,7 @@ namespace Story
         [SerializeField] private StoryListButton storyListButton;
         [SerializeField] private Button backToMenuButton;
         [SerializeField] private Canvas canvas;
+        private int storyId;
 
         void Start()
         {
@@ -23,14 +24,25 @@ namespace Story
         {
         }
 
-        void LoadStoryScene()
-        {
-            SceneManager.LoadScene("StoryScene");
-        }
-
         void LoadMenuScene()
         {
             SceneManager.LoadScene("MenuScene");
+        }
+
+        void LoadStoryScene(int storyId)
+        {
+            SceneManager.sceneLoaded += StorySceneLoaded;
+            SceneManager.LoadScene("StoryScene");
+            this.storyId = storyId;
+        }
+
+        private void StorySceneLoaded(Scene next, LoadSceneMode mode)
+        {
+            var controller = GameObject.FindWithTag("StoryControllerObject")
+                .GetComponent<StoryController>();
+            controller.storyId = storyId;
+            // イベントから削除
+            SceneManager.sceneLoaded -= StorySceneLoaded;
         }
 
         void SetupStoryListView()
@@ -44,7 +56,7 @@ namespace Story
                 text.text = story.title;
                 storyListButtonObj.transform.localPosition = new Vector3(0, -50*i);
                 var button = storyListButtonObj.GetComponent<Button>();
-                button.onClick.AddListener(LoadStoryScene);
+                button.onClick.AddListener(()=>LoadStoryScene(story.id));
                 storyListButtonObj.transform.SetParent(canvas.transform, false);
                 i++;
             }
