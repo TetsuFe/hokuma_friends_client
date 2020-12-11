779ba402f7e6317abf09850f97801141d150ef74
TetsuFe <tetsubeg1ner27@gmail.com>
2020-12-12 03:28:44 +0900

    Use real api data in master data api client

--- Assets/Scripts/DataVersion/MasterDataVersionApi.cs
+++ Assets/Scripts/DataVersion/MasterDataVersionApi.cs
@@ -1,12 +1,22 @@
+using System;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace DataVersion
 {
+    [Serializable]
+    public class StoryMasterDataVersion
+    {
+        public int version;
+    }
     public class MasterDataVersionApi
     {
-        public async UniTask<int> GetMasterDataVersion()
+        public async UniTask<int> GetStoryMasterDataVersion()
         {
-            return 2;
+            var request = UnityWebRequest.Get("http://localhost:8001/api/storyMasterDataVersion/");
+            await request.SendWebRequest();
+            return JsonUtility.FromJson<StoryMasterDataVersion>(request.downloadHandler.text).version;
         }
     }
 }
