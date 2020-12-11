5920d175bcca688bc22dd1d12c03563e48536f03
TetsuFe <tetsubeg1ner27@gmail.com>
2020-12-11 19:51:25 +0900

    Add master story data download if local version is lower than master version

---
+++ Assets/Scripts/Story/StoryApi.cs
@@ -0,0 +1,28 @@
+using System;
+using api;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Story
+{
+    [Serializable]
+    class Stories
+    {
+        public Story2[] stories;
+    }
+
+    public class StoryApi
+    {
+        public async UniTask<Story2[]> GetAll()
+        {
+            // var request = UnityWebRequest.Get(ApiHostName.instance.hostName + "/api/stories");
+            var request = UnityWebRequest.Get("http://localhost:8001" + "/api/stories");
+            await request.SendWebRequest();
+            var stories = JsonUtility.FromJson<Stories>(request.downloadHandler.text);
+            Debug.Log(request.downloadHandler.text);
+            Debug.Log(stories);
+            return stories.stories;
+        }
+    }
+}
